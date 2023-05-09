24cb1a7267e176d3830b3d940ac74afee9b2a73e
LeoValleWeb <leomartinsvalle@gmail.com>
2023-05-09 13:22:51 +0000

    save

---
+++ Aluno.java
@@ -0,0 +1,39 @@
+
+
+public class Aluno extends Pessoa {
+
+    private String semestre;
+    private String curso;
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+
+    public String getSemestre(){
+        return semestre;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public Aluno(String nome, String endereco, int idade, String curso, String semestre){
+        super(nome, idade, endereco);
+        this.curso = curso;
+        this.semestre = semestre;
+    }
+
+    public void dados() {
+        System.out.println("Nome do Aluno: " + getNome());
+        System.out.println("Idade do Aluno: " + getIdade());
+        System.out.println("Endereço do aluno: " + getEndereco());
+        System.out.println("Curso do Aluno: " + getCurso());
+        System.out.println("Semestre atual: " + getSemestre() + "\n");
+
+        
+    }
+}
---
+++ Pessoa.java
@@ -0,0 +1,35 @@
+public class Pessoa{
+    private String nome;
+    private int idade;
+    private String endereco;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public Pessoa(String nome, int idade, String endereco){
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+}
